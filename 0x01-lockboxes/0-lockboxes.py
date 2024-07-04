b9595e952c128d94643381656bac04e368fecb02
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-07-04 04:00:48 -0700

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """ Lockboxes"""
+from collections import deque
 
 
 def canUnlockAll(boxes):
     """Lockboxes"""
     n = len(boxes)
-    unlocked = set()
-    keys = set([0])
+    unlocked = set([0])
+    keys = deque([0])  # Use a deque for more efficient popping from the left
 
     while keys:
-        key = keys.pop()
-        if key not in unlocked:
-            unlocked.add(key)
-            for k in boxes[key]:
-                if k not in keys:
-                    keys.add(k)
+        key = keys.popleft()  # Efficiently pop from the left
+        for k in boxes[key]:
+            if k not in unlocked and k < n:
+                unlocked.add(k)
+                keys.append(k)
 
     return len(unlocked) == n
