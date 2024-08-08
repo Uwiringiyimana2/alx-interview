ef5464e8e3ad2844a3f640c27391c160122dfab1
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-08-08 10:45:38 -0700

    API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,62 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/';
 
-const movieID = process.argv[2];
-
-function getMovie (movieID, callback) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Failed to fetch data:', error);
-      return callback(error, null);
+if (process.argv.length > 2) {
+  request(`${API_URL}${process.argv[2]}`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
-    if (response.statusCode === 200) {
-      try {
-        const data = JSON.parse(body);
-        callback(null, data);
-      } catch (err) {
-        console.error('Failed to parse JSON:', err);
-        callback(err, null);
-      }
-    } else {
-      console.error('Request failed with status code:', response.statusCode);
-      callback(new Error(`Request failed with status code ${response.statusCode}`), null);
-    }
-  });
-}
-
-function printCharacters (movieID) {
-  getMovie(movieID, (error, data) => {
-    if (error) {
-      console.error('Error:', error);
-      return;
-    }
-    const characters = data.characters;
-
-    if (!characters || characters.length === 0) {
-      console.log('No characters found.');
-      return;
-    }
-
-    characters.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Failed to fetch character:', error);
-          return;
-        }
-        if (response.statusCode === 200) {
-          try {
-            const charData = JSON.parse(body);
-            console.log(charData.name);
-          } catch (err) {
-            console.error('Failed to parse character JSON:', err);
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, _, charReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
           }
-        } else {
-          console.error('Failed to fetch character:', response.statusCode);
-        }
-      });
-    });
+          resolve(JSON.parse(charReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
-
-printCharacters(movieID);
